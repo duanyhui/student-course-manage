f7ba5ca7687f02068c1c4ceda2c70af7c3ad6e5c
duanyhui <91049287+duanyhui@users.noreply.github.com>
2022-12-01 22:55:11 +0800

    我的评价是一伯昏

---
+++ server/src/main/java/duan/server/mapper/SchoolyearMapper.java
@@ -0,0 +1,18 @@
+package duan.server.mapper;
+
+import duan.server.entity.Schoolyear;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author duanyhui
+ * @since 2022-10-24
+ */
+@Mapper
+public interface SchoolyearMapper extends BaseMapper<Schoolyear> {
+
+}
